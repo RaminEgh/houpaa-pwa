0ef5728bfbda9991ed2aec54a7ebeeb66171c3f0
Ramin Eghbalian Khob <Ramin Eghbalian Khob>
2021-09-17 12:45:56 +0430

    show users list in home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,68 @@
-import React from 'react';
-
+import React, {useEffect, useState} from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
 const Home = () => {
+
+    const [data, setData] = useState([]);
+    const [mode, setMode] = useState('online');
+    useEffect(() => {
+        const url = 'https://reqres.in/api/users';
+        fetch(url).then((response) => {
+            response.json().then((result) => {
+                console.warn(result)
+                setData(result.data)
+                localStorage.setItem('users', JSON.stringify(result))
+            })
+        }).catch(error => {
+            setMode('offline')
+            const collection = JSON.parse(localStorage.getItem('users'))
+            setData(collection.data);
+        })
+    }, [])
+
+
     return (
-        <div>
-            HOME page
-        </div>
+        <>
+            {
+                mode === 'offline' ? <strong>You are in offline mode</strong> : null
+            }
+            <List sx={{ width: '100%', bgcolor: 'background.paper' }} folders>
+                {
+                    data.map(item => {
+                        return <React.Fragment key={item.id}>
+                            <ListItem alignItems="flex-start" button>
+                                <ListItemAvatar>
+                                    <Avatar alt="Remy Sharp" src={item.avatar} />
+                                </ListItemAvatar>
+                                <ListItemText
+                                    primary="Brunch this weekend?"
+                                    secondary={
+                                        <React.Fragment>
+                                            <Typography
+                                                sx={{ display: 'inline' }}
+                                                component="span"
+                                                variant="body2"
+                                                color="text.primary"
+                                            >
+                                                {`${item.first_name} ${item.last_name}`}
+                                            </Typography>
+                                            {" — I'll be in your neighborhood doing errands this…"}
+                                        </React.Fragment>
+                                    }
+                                />
+                            </ListItem>
+                            <Divider variant="inset" component="li" />
+                        </React.Fragment>
+                    })
+                }
+
+            </List>
+        </>
     );
 };
 
